681b6d1f80df77bd7bef8201f2a6d7e06d691c5a
Xavier <xavier@Xaviers-MacBook-Pro.local>
2019-04-07 22:24:43 +0800

    modify a bug.

--- java/microservices/composite/productcomposite/src/main/java/com/zhkui/composite/product/service/ProductCompositeService.java
+++ java/microservices/composite/productcomposite/src/main/java/com/zhkui/composite/product/service/ProductCompositeService.java
@@ -7,5 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProductCompositeService {
-    SpringApplication.run(ProductCompositeService.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCompositeService.class, args);
+    }
 }
