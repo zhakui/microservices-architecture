8eef1308bf9181d3939eaaded8db80d6c6692f7f
zhakui <zh-ge@163.com>
2019-04-17 21:20:55 +0800

    .

---
+++ java/microservices/core/product-service/src/main/java/com/zhkui/core/product/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zhkui.core.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
