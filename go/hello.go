249ba0fd4a6462681b61fc8dbfc611f22462d59b
Xavier <xavier@Xaviers-MacBook-Pro.local>
2019-05-05 21:12:20 +0800

    debug

---
+++ go/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"./name"
+)
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("hel")
+	fmt.Println(name.GetName("zhangkui"))
+}
---
+++ go/name/name.go
@@ -0,0 +1,5 @@
+package name
+
+func GetName(name string) string {
+	return name
+}
