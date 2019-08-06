099036fe01612523c6ad3720968f97d036847a07
xavier <zh-ge@163.com>
2019-08-06 23:22:56 +0800

    add workspace file

--- java/microservices/composite/productcomposite/src/main/java/com/zhkui/composite/product/ProductCompositeServiceApplication.java
+++ java/microservices/composite/productcomposite/src/main/java/com/zhkui/composite/product/ProductCompositeServiceApplication.java
@@ -2,18 +2,12 @@ package com.zhkui.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-=======
->>>>>>> 1badb4e2d4fd7770aa200a15a31b7657a79f77a7
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-<<<<<<< HEAD
 @EnableCircuitBreaker
-=======
->>>>>>> 1badb4e2d4fd7770aa200a15a31b7657a79f77a7
 public class ProductCompositeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductCompositeServiceApplication.class, args);
--- java/microservices/support/edge-service/src/main/java/com/zhkui/support/edge/ZuulApplication.java
+++ java/microservices/support/edge-service/src/main/java/com/zhkui/support/edge/ZuulApplication.java
@@ -1,27 +1,13 @@
 package com.zhkui.support.edge;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-=======
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
-
-@SpringBootApplication
-@Controller
->>>>>>> 1badb4e2d4fd7770aa200a15a31b7657a79f77a7
 @EnableZuulProxy
-public class ZuulApplication {
-	
+public class ZuulApplication {	
     public static void main(String[] args) {
-<<<<<<< HEAD
         SpringApplication.run(ZuulApplication.class,args);
-=======
-        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
->>>>>>> 1badb4e2d4fd7770aa200a15a31b7657a79f77a7
     }
 }
